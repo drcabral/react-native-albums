d4e81e6e2dd4056626689ed5c6050d41efb18cee
Diogo Cabral <drkbral@gmail.com>
2017-01-13 01:13:01 -0300

    Adicionando componentes externos e realizando export/import

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+//make a Component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//make the component available to other parts of the App
+export default Header;
