4eecda10430c5c792be37531629fcef3f84561ae
Diogo Cabral <drkbral@gmail.com>
2017-01-17 02:03:00 -0300

    Finalizando o projeto Albums.

--- index.android.js
+++ index.android.js
@@ -1,11 +1,16 @@
 //import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 //create a Component
 const App = () => (
-  <Header />
+  //flex 1 here to root view support all of content
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 //render it to the device
